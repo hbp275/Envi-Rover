44c1a76effff41fe876d6bc627f2a978e13af112
hbp275 <hbpatil275@gmail.com>
2025-04-27 15:00:55 +0530

    Add files via upload

--- receiver_rover/receiver.ino
+++ receiver_rover/receiver.ino
@@ -1,82 +1,137 @@
 #include <SPI.h>
 #include <LoRa.h>
-#include <Servo.h>
 #include <DHT.h>
 
+// Define LoRa module pins
 #define LORA_SS 10
 #define LORA_RST 9
 #define LORA_DIO0 2
 
-#define SERVO_PIN 3
-
-// DHT Sensor setup
-#define DHTPIN 4
+// Define DHT11
+#define DHTPIN 3
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Ultrasonic sensor
-#define TRIG_PIN 5
-#define ECHO_PIN 6
-
-Servo myServo;
+// Motor driver pins
+#define IN1 4
+#define IN2 5
+#define ENA 6
+#define IN3 7
+#define IN4 8
+#define ENB 11
 
 void setup() {
-  Serial.begin(9600);
-  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+  // No Serial.begin()
 
-  if (!LoRa.begin(433E6)) {
-    Serial.println("LoRa init failed!");
-    while (1);
-  }
+  // Set motor pins as OUTPUT
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
 
-  myServo.attach(SERVO_PIN);
+  // Start motors disabled
+  stopMotors();
+  
+  // Enable PWM always ON (important)
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 150);
+
   dht.begin();
-  pinMode(TRIG_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
 
-  Serial.println("LoRa Rover Ready!");
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+  if (!LoRa.begin(433E6)) {
+    while (1); // Stay here if LoRa fails
+  }
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    String receivedData = "";
+  if (packetSize > 0) {
+    String incoming = "";
     while (LoRa.available()) {
-      receivedData += (char)LoRa.read();
+      incoming += (char)LoRa.read();
     }
+    incoming.trim();  // Remove \n \r spaces
 
-    if (receivedData == "m") {
-      // Sensor data requested
-      float temp = dht.readTemperature();
-      float hum = dht.readHumidity();
-      long duration;
-      float distance;
+    if (incoming.length() > 0) {
+      char command = incoming.charAt(0);  // Take only first clean character
 
-      digitalWrite(TRIG_PIN, LOW);
-      delayMicroseconds(2);
-      digitalWrite(TRIG_PIN, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TRIG_PIN, LOW);
-      duration = pulseIn(ECHO_PIN, HIGH);
-      distance = duration * 0.034 / 2;
-
-      String sensorData = "T:" + String(temp) + "C, H:" + String(hum) + "%, D:" + String(distance) + "cm";
-      Serial.println("Sending: " + sensorData);
-
-      LoRa.beginPacket();
-      LoRa.print(sensorData);
-      LoRa.endPacket();
-    } else {
-      // Servo control
-      int receivedAngle = receivedData.toInt();
-      if (receivedAngle >= 0 && receivedAngle <= 180) {
-        myServo.write(receivedAngle);
-        Serial.println("Received Angle: " + String(receivedAngle));
-      } else {
-        Serial.println("Invalid Angle Received");
+      if (command == 'o') {
+        sendSensorData();
+      } else if (isValidMotorCommand(command)) {
+        controlMotors(command);
       }
     }
   }
+}
 
-  delay(100);
+bool isValidMotorCommand(char c) {
+  return (c == 'w' || c == 'a' || c == 's' || c == 'd' || c == 'x');
+}
+
+void controlMotors(char command) {
+  // Always keep ENA and ENB powered
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
+
+  switch (command) {
+    case 'w': // Forward
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+    
+
+    case 's': // Backward
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+
+    case 'a': // Turn Left
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+
+    case 'd': // Turn Right
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+
+    case 'x': // Stop
+      stopMotors();
+      break;
+  }
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void sendSensorData() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  String sensorData = "";
+
+  if (isnan(humidity) || isnan(temperature)) {
+    sensorData = "Error: DHT Sensor!";
+  } else {
+    sensorData = "Humidity:" + String(humidity, 1) + "% Temp:" + String(temperature, 1) + "C";
+  }
+
+  LoRa.beginPacket();
+  LoRa.print(sensorData);
+  LoRa.endPacket();
 }
